ad731cf9fc575c87d1b36751b09d3fca4bc829e8
hai.nguyen <haingdc@gmail.com>
2021-03-07 22:31:01 +0700

    update web-cra config

--- packages/web-cra/config-overrides.js
+++ packages/web-cra/config-overrides.js
@@ -13,6 +13,7 @@ module.exports = function override(config, env) {
   config.resolve.plugins = config.resolve.plugins.filter(
     (plugin) => plugin.constructor.name !== 'ModuleScopePlugin',
   )
+  // fs.writeFile('todo.txt', JSON.stringify(config.module.rules, null, 4), function() {})
 
   config.module.rules[0].include = appIncludes
   config.module.rules[1].oneOf[2].include = appIncludes
@@ -23,6 +24,37 @@ module.exports = function override(config, env) {
   config.plugins.push(
     new webpack.DefinePlugin({ __DEV__: env !== 'production' }),
   )
+
+  // config.module.rules.push({
+  //   test: /\.(ttf)$/i,
+  //   loader: "url-loader", // or directly file-loader
+  //   include: path.resolve("node_modules/react-native-vector-icons"),
+  // })
+
+  // fs.writeFile('todo2.txt', path.resolve(__dirname, "../../node_modules/react-native-vector-icons"), function noop() {})
+
+  config.module.rules.push({
+    test: /\.js$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        // Disable reading babel configuration
+        babelrc: false,
+        configFile: false,
+
+        // The configration for compilation
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react',
+          '@babel/preset-flow'
+        ],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-object-rest-spread'
+        ]
+      }
+    }
+  })
 
   return config
 }
