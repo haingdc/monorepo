c3a5523912eecc9e42e615e51221d40ada0a6a3c
hai.nguyen <haingdc@gmail.com>
2021-03-07 22:37:38 +0700

    use expo/blur

--- packages/components/src/pages/wishlist/index.tsx
+++ packages/components/src/pages/wishlist/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, ScrollView, View, Image } from 'react-native';
+import { BlurView } from 'expo-blur';
 import { ProductList } from '../../containers/product-list';
 import cursor   from '../../../assets/cursor.png';
 import chevron  from '../../../assets/chevron-right.png';
 import cardPic1 from '../../../assets/card_s_taj-vista.png';
 import cardPic2 from '../../../assets/card_s_sheraton.png';
-
-
 
 export const list = [
   { title: 'Sheraton Grand', source: cardPic1, price: '5999' },
@@ -30,12 +29,17 @@ export function Wishlist() {
       >
         <View nativeID="todoBanner" style={bannerStyles.container}>
           <Image style={bannerStyles.cursor} source={cursor}/>
-          <Text style={bannerStyles.text}>We have deals for your favorite and relaxing stay. </Text>
+          <View>
+            <Text style={bannerStyles.text}>We have deals for</Text>
+            <Text style={bannerStyles.text}>your favorite and relaxing</Text>
+            <Text style={bannerStyles.text}>stay.</Text>
+          </View>
           <ViewButton style={bannerStyles.cta} />
         </View>
         <ProductList data={list} key="Hotels1" onViewAll={() => {}}>Top Hotels</ProductList>
         <ProductList data={list} key="Hotels2" onViewAll={() => {}}
                      style={{ marginVertical: 20, marginHorizontal: 20 }}>Top Deals</ProductList>
+        <Todo />
       </View>
     </ScrollView>
   );
@@ -45,10 +49,10 @@ export function Wishlist() {
 function ViewButton(props) {
   const style = StyleSheet.compose(viewButtonStyles.container, props.style);
   return (
-    <View nativeID="todoView" style={style}>
+    <BlurView  nativeID="todoView" style={style} intensity={100}>
       <Text style={viewButtonStyles.text}>View All</Text>
       <Image style={viewButtonStyles.image} source={chevron} />
-    </View>
+    </BlurView>
   )
 }
 
@@ -80,7 +84,7 @@ const bannerStyles = StyleSheet.create({
     borderRadius   : 4,
     flexDirection  : 'row',
     backgroundColor: '#EF4339',
-    marginHorizontal: 20,
+    width: 326,
     paddingLeft: 8,
     paddingTop: 12,
     paddingBottom: 5,
@@ -90,7 +94,6 @@ const bannerStyles = StyleSheet.create({
     lineHeight   : 30,
     letterSpacing: 1.1,
     color:    '#fff',
-    paddingRight: 28,
   },
   cursor: {
     width : 48,
@@ -103,3 +106,48 @@ const bannerStyles = StyleSheet.create({
     position: 'absolute',
   },
 });
+
+
+
+
+const uri = 'https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png';
+
+export function Todo() {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        {/* <Image style={styles.blurredImage} source={cardPic1} /> */}
+
+        {/* Adjust the tint and intensity */}
+        <BlurView intensity={10} style={[StyleSheet.absoluteFill, styles.nonBlurredContent]}
+          tint="dark"
+        >
+          <Text>Hello! I am bluring contents underneath</Text>
+        </BlurView>
+        <Text style={styles.out}>Hello! I am bluring contents underneath</Text>
+      </View>
+    </View>
+  );
+}
+
+/* @hide const styles = StyleSheet.create({ ... }); */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  blurredImage: {
+    width: 192,
+    height: 192,
+  },
+  nonBlurredContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  out: {
+    height: 100,
+    color: 'red',
+  },
+});
+/* @end */
--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,15 +1,22 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+// /* eslint-disable @typescript-eslint/no-var-requires */
 
-const path = require('path')
+// const path = require('path')
 
-module.exports = {
-  watchFolders: [path.resolve(__dirname, '../../')],
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-}
+// module.exports = {
+//   watchFolders: [path.resolve(__dirname, '../../')],
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: false,
+//       },
+//     }),
+//   },
+// }
+
+
+
+// eslint-disable-next-line
+const { createMetroConfiguration } = require("expo-yarn-workspaces");
+
+module.exports = createMetroConfiguration(__dirname);
