9ecd91dd9df77e20fc19d9c5535c39578d0ea157
hai.nguyen <haingdc@gmail.com>
2021-03-08 08:06:03 +0700

    update style

--- packages/components/src/pages/wishlist/index.tsx
+++ packages/components/src/pages/wishlist/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, ScrollView, View, Image } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, Text, ScrollView, View, Image, Platform } from 'react-native';
 import { BlurView } from 'expo-blur';
 import { ProductList } from '../../containers/product-list';
 import cursor   from '../../../assets/cursor.png';
@@ -29,7 +29,7 @@ export function Wishlist() {
       >
         <View nativeID="todoBanner" style={bannerStyles.container}>
           <Image style={bannerStyles.cursor} source={cursor}/>
-          <View>
+          <View style={bannerStyles.textWrapper}>
             <Text style={bannerStyles.text}>We have deals for</Text>
             <Text style={bannerStyles.text}>your favorite and relaxing</Text>
             <Text style={bannerStyles.text}>stay.</Text>
@@ -39,7 +39,6 @@ export function Wishlist() {
         <ProductList data={list} key="Hotels1" onViewAll={() => {}}>Top Hotels</ProductList>
         <ProductList data={list} key="Hotels2" onViewAll={() => {}}
                      style={{ marginVertical: 20, marginHorizontal: 20 }}>Top Deals</ProductList>
-        <Todo />
       </View>
     </ScrollView>
   );
@@ -47,30 +46,44 @@ export function Wishlist() {
 
 
 function ViewButton(props) {
+  const blurRef = useRef({
+    tint     : Platform.OS == 'ios' ? 'light' : 'default',
+    intensity: Platform.OS == 'ios' ?      10 : 100,
+  })
   const style = StyleSheet.compose(viewButtonStyles.container, props.style);
   return (
-    <BlurView  nativeID="todoView" style={style} intensity={100}>
-      <Text style={viewButtonStyles.text}>View All</Text>
-      <Image style={viewButtonStyles.image} source={chevron} />
-    </BlurView>
+    <View style={style}>
+      <BlurView
+        tint = {blurRef.current.tint}
+        style = {viewButtonStyles.blurview}
+        intensity = {blurRef.current.intensity}
+      >
+        <Text style={viewButtonStyles.text}>View</Text>
+        <Image style={viewButtonStyles.image} source={chevron} />
+      </BlurView>
+    </View>
   )
 }
 
 const viewButtonStyles = StyleSheet.create({
   container: {
     borderRadius: 4,
+    overflow: 'hidden',
+  },
+  blurview: {
     flexDirection: 'row',
-    backgroundColor: 'lightgreen',
     paddingLeft: 26,
     paddingRight: 18,
-    paddingVertical: 11,
+    paddingVertical: 9,
     alignItems: 'center',
+    width: '100%',
+    height: '100%',
   },
   text: {
     color: '#fff',
     fontSize: 16,
     lineHeight: 19,
-    letterSpacing: 1.1,
+    // letterSpacing: 1.1,
     marginRight: 9,
   },
   image: {
@@ -85,17 +98,22 @@ const bannerStyles = StyleSheet.create({
     flexDirection  : 'row',
     backgroundColor: '#EF4339',
     width: 326,
-    paddingLeft: 8,
     paddingTop: 12,
-    paddingBottom: 5,
+    paddingBottom: 7,
+    paddingLeft: 5,
+    marginBottom: 15,
+  },
+  textWrapper: {
   },
   text: {
     fontSize     : 20,
     lineHeight   : 30,
-    letterSpacing: 1.1,
+    // letterSpacing: 1.1,
     color:    '#fff',
+    flex: 0,
   },
   cursor: {
+    flex: 0,
     width : 48,
     height: 50,
     marginRight: 20,
@@ -106,48 +124,3 @@ const bannerStyles = StyleSheet.create({
     position: 'absolute',
   },
 });
-
-
-
-
-const uri = 'https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png';
-
-export function Todo() {
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.container}>
-        {/* <Image style={styles.blurredImage} source={cardPic1} /> */}
-
-        {/* Adjust the tint and intensity */}
-        <BlurView intensity={10} style={[StyleSheet.absoluteFill, styles.nonBlurredContent]}
-          tint="dark"
-        >
-          <Text>Hello! I am bluring contents underneath</Text>
-        </BlurView>
-        <Text style={styles.out}>Hello! I am bluring contents underneath</Text>
-      </View>
-    </View>
-  );
-}
-
-/* @hide const styles = StyleSheet.create({ ... }); */
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  blurredImage: {
-    width: 192,
-    height: 192,
-  },
-  nonBlurredContent: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  out: {
-    height: 100,
-    color: 'red',
-  },
-});
-/* @end */
